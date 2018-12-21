61a4fd4ba9eeba0a0b449a82e24203ec3f2af17d
Chetan Jadhav <46045257+cjedit@users.noreply.github.com>
2018-12-21 15:24:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>DA’LAAYEE ENTERPRISES</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+   <?php include("menu.php") ?>
+
+    <!-- ##### Hero Area Start ##### -->
+    <div class="hero-area owl-carousel">
+        <!-- Single Blog Post -->
+        <div class="hero-blog-post bg-img bg-overlay" style="background-image: url(img/bg-img/1.jpg);">
+            
+        </div>
+
+        <!-- Single Blog Post -->
+        <div class="hero-blog-post bg-img bg-overlay" style="background-image: url(img/bg-img/2.jpg);">
+            
+        </div>
+
+        <!-- Single Blog Post -->
+        <div class="hero-blog-post bg-img bg-overlay" style="background-image: url(img/bg-img/3.jpg);">
+           
+        </div>
+		<div class="hero-blog-post bg-img bg-overlay" style="background-image: url(img/bg-img/4.jpg);">
+           
+        </div>
+    </div>
+    <!-- ##### Hero Area End ##### -->
+
+	<section class="py-5 hm-wel">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 p-30 box-shadow">
+				 <div class="section-heading">
+                            <h1>Welcome to DA’LAAYEE ENTERPRISES </h1>
+                        </div>
+				<p>DA’LAAYEE ENTERPRISES founded in the year 2018. The firm specializes in the areas of LEGAL, CORPORATE LAWS, INTELLECTUAL PROPERTY RIGHTS (IPR) in Domestic and International level And other Company registration and certification. The firm provides litigation, transactional, advisory, consulting and legal services to its clients under each of its practice areas. The strength of the firm is its experienced and diverse human resource. The firm has attorneys and professionals with advanced knowledge in area of LEGAL | IPR | GOVERNMENT SECTOR’S.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	
+    <!-- ##### Mag Posts Area Start ##### -->
+    <section class="mag-posts-area d-flex flex-wrap py-5">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-heading">
+                        <h2>Our Services </h2>
+                    </div>
+				</div>
+				<div class="col-md-6">
+					<div class="single-team-member d-flex align-items-center box-shadow">
+                            <div class="team-member-thumbnail">
+                                <img src="img/bg-img/hm-2.jpg" alt="">
+                               
+                            </div>
+                            <div class="team-member-content">
+                                <h6>FIRM & COMPANY REGISTRATION</h6>                                
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur mauris id scelerisque eleifend. Nunc vestibulum cursea quam at scelerisque.</p>
+								<span>Read More</span>
+							</div>
+                        </div>
+				</div>
+				<div class="col-md-6">
+					<div class="single-team-member d-flex align-items-center box-shadow">
+                            <div class="team-member-thumbnail">
+                                <img src="img/bg-img/hm-3.jpg" alt="">
+                               
+                            </div>
+                            <div class="team-member-content">
+                                <h6>IPR SERVICES</h6>                                
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur mauris id scelerisque eleifend. Nunc vestibulum cursea quam at scelerisque.</p>
+								<span>Read More</span>
+							</div>
+                        </div>
+				</div>
+				<div class="col-md-6">
+					<div class="single-team-member d-flex align-items-center box-shadow">
+                            <div class="team-member-thumbnail">
+                                <img src="img/bg-img/hm-4.jpg" alt="">
+                               
+                            </div>
+                            <div class="team-member-content">
+                                <h6>OTHER SERVICES</h6>                                
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur mauris id scelerisque eleifend. Nunc vestibulum cursea quam at scelerisque.</p>
+								<span>Read More</span>
+							</div>
+                        </div>
+				</div>
+				<div class="col-md-6">
+					<div class="single-team-member d-flex align-items-center box-shadow">
+                            <div class="team-member-thumbnail">
+                                <img src="img/bg-img/hm-1.jpg" alt="">
+                               
+                            </div>
+                            <div class="team-member-content">
+                                <h6>PHOTOGRAPHY</h6>                                
+                                <p>Industrial product photography for patent / design registration and promotional purpose.</p>
+								<span>Read More</span>
+							</div>
+                        </div>
+				</div>
+			</div>
+        </div>
+    </section>
+	
+    <!-- ##### Mag Posts Area End ##### -->
+  <?php include("footer.php") ?>
